dcc0448b4380e164068f8150762fb1fc586c947b
lutzda <67082129+lutzda@users.noreply.github.com>
2023-05-17 17:38:33 -0400

    Rev_A
    Adds a JSON output and reduces the content of the card returned.

--- main.py
+++ main.py
@@ -19,27 +19,34 @@ f = open('default-cards.json', errors="ignore")
 #create the linked list
 llist = LinkedList()
 
+
 #cards counts the number of cards read into the linked list
 cards = 0
 
 #for loop that runs through the JSON file opened above and creates the linked list for only vintage legal cards.
 for card in ijson.items(f,"item"):
     if card["legalities"]["vintage"] == "legal":
+        #'reduced_card' is used b/c there are illegal characters in the full file.
+        reduced_card = {}
+        reduced_card["id"] = card["id"]
+        reduced_card["name"] = card["name"]
+        reduced_card["released_at"] = card["released_at"]
+        reduced_card["uri"] = card["uri"]
         cards += 1
         if cards == 1: #this is the first card in the list so the card becomes the linked list head
-            node = Node(card)
+            node = Node(reduced_card)
             llist.head = node
             previous_node = node
         else:
-            node = Node(card)
+            node = Node(reduced_card)
             previous_node.next = node
             previous_node = node
 f.close()
-
 #starts an infinite 'while' loop.
 while True:
     time.sleep(1)
     fp = open('transfer.txt', 'r+')
+    fp_json = open('transfer.json', 'r+')
     line = fp.read() #reads the first line from the open text file
     if line == "run": 
         random_number = random.randrange(1, cards) #creates a random number from 1 to the number of cards in the linked list
@@ -48,8 +55,16 @@ while True:
         while i < random_number: #cycles through each card in the linked list until i is the random number.
             random_card = random_card.next
             i += 1
-        fp.seek(0) #goes back to the start of the transfer.txt file
-        fp.truncate() #deletes the content in the transfer.txt file
+
+        #delete the contents from both files
+        fp.seek(0)
+        fp.truncate()
+        fp_json.seek(0)
+        fp_json.truncate()
+
+
         print(random_card.data) #prints the random_card to the console (this can be removed if desired)
-        fp.write(str(random_card.data)) #puts the card data into the text file as a string
+        json.dump(random_card.data, fp, indent=4)
+        json.dump(random_card.data, fp_json, indent=4)
     fp.close()
+    fp_json.close()
